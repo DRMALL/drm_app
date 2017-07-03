56126b245bf5f1e28cce871084020b9e2a35c2ad
joephon <306519779@qq.com>
2017-07-04 04:13:54 +0800

    fixed

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ export default class drm_app extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!!!!
+          i am joephon!aha!!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
