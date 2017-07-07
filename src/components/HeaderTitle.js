6000d962b47fd720cdd34268545603c3be507adb
joephon <306519779@qq.com>
2017-07-08 04:19:58 +0800

    added title render logic

--- src/components/HeaderTitle.js
+++ src/components/HeaderTitle.js
@@ -3,14 +3,20 @@ import { View, Text, Image } from 'react-native'
 
 export default ({ navigation }) => {
   const title = (navigation => {
-    if (navigation.state.index == 0) return '设备管理系统'
-    else if(navigation.state.index == 1) return '设备档案'
-    else if(navigation.state.index == 2) return '设备监控'
-    else return <Text>ooxx</Text>
+    if (navigation.state.index == 0) return <Title title='设备管理系统' />
+    else if(navigation.state.index == 1) return <Title title='设备档案' />
+    else if(navigation.state.index == 2) return <Title title='设备监控' />
+    else return <Text>There will be a search bar</Text>
   })(navigation)
   return(
     <View style={{width: '100%'}}>
-      <Text style={{color: '#fff', fontWeight: 'bold', fontSize: 18, textAlign:'center'}}>{title}</Text>
+      {title}
     </View>
+  )
+}
+
+const Title = props => {
+  return(
+    <Text style={{color: '#fff', fontWeight: 'bold', fontSize: 18, textAlign:'center'}}>{props.title}</Text>
   )
 }
