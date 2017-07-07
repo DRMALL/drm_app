699fbecd3abac34dec3600e576684e06541c1132
joephon <306519779@qq.com>
2017-07-08 04:14:09 +0800

    fixed sth

--- src/components/HeaderTitle.js
+++ src/components/HeaderTitle.js
@@ -2,16 +2,15 @@ import React from 'react'
 import { View, Text, Image } from 'react-native'
 
 export default ({ navigation }) => {
-  console.log(navigation)
-  const title = () => {
+  const title = (navigation => {
     if (navigation.state.index == 0) return '设备管理系统'
     else if(navigation.state.index == 1) return '设备档案'
     else if(navigation.state.index == 2) return '设备监控'
     else return <Text>ooxx</Text>
-  }
+  })(navigation)
   return(
     <View style={{width: '100%'}}>
-      <Text style={{color: '#fff', fontWeight: 'bold', fontSize: 18, textAlign:'center'}}>{title()}</Text>
+      <Text style={{color: '#fff', fontWeight: 'bold', fontSize: 18, textAlign:'center'}}>{title}</Text>
     </View>
   )
 }
