6df972a70545eb92f705639fcfaec198e8827dc8
joephon <306519779@qq.com>
2017-07-12 00:28:29 +0800

    added some logic

--- src/components/HeaderTitle.js
+++ src/components/HeaderTitle.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { View, Text, Image } from 'react-native'
 
-export default ({ navigation }) => {
-  const title = (navigation => {
+export default ({ navigation, navigate }) => {
+  const title = ((navigation, navigate) => {
     if (navigation.state.index == 0) return <Title title='设备管理系统' />
     else if(navigation.state.index == 1) return <Title title='设备档案' />
     else if(navigation.state.index == 2) return <Title title='设备监控' />
-    else return <Text>There will be a search bar</Text>
+    else return <Search navigate={navigate} />
   })(navigation)
   return(
     <View style={{width: '100%'}}>
@@ -18,5 +18,15 @@ export default ({ navigation }) => {
 const Title = props => {
   return(
     <Text style={{color: '#fff', fontWeight: 'bold', fontSize: 18, textAlign:'center'}}>{props.title}</Text>
+  )
+}
+
+const Search = props => {
+  return(
+    <View 
+      onPress={() => props.navigate('Home')}
+      style={{width:'100%', height:'70%',marginTop:5,borderRadius:3,backgroundColor: '#fff'}}>
+
+    </View>
   )
 }
