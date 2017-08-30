94067d7818468552c7c8f5a20c034216e37bedea
Cbinbin <1142293069@qq.com>
2017-08-30 20:55:19 +0800

    rich editor module install

--- android/app/src/main/java/com/drm_app/MainApplication.java
+++ android/app/src/main/java/com/drm_app/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.github.alinz.reactnativewebviewbridge.WebViewBridgePackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,7 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new SplashScreenReactPackage(),
-            new PickerPackage()
+            new PickerPackage(),
+            new WebViewBridgePackage()    //richTextEditor
       );
     }
   };
